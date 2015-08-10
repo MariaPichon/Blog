7954f6304d0970f23935d5f8f6a3bae65349a6c4
Maria Isabel Pichon Cadena <mariapichon4@gmail.com>
2015-08-10 16:51:04 -0500

    First Blog Post

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/Blog/"
+http_images_path = "/Blog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/Blog/fonts"
+css_dir = "public/Blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2015-08-10-first-post.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "First Post"
+date: 2015-08-10 16:44:35 -0500
+comments: true
+categories: 
+---
+
+## First Blog Post ##
